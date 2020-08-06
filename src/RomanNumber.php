44c471cf483476684cac6ccc9837eb3a7bd80e6d
Santiago Correa Muñoz <scorrea153@gmail.com>
2020-08-06 17:19:37 +0200

    Kata numeros romanos

--- src/RomanNumber.php
+++ src/RomanNumber.php
@@ -4,8 +4,30 @@ namespace App;
 
 class RomanNumber
 {
+    const ROMAN_NUMERALS = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    ];
+
     public function get(int $number)
     {
-        return 'I';
+        $result = '';
+        foreach(self::ROMAN_NUMERALS as $roman => $arabic){
+            for(; $number >= $arabic; $number -= $arabic) {
+                $result .= $roman;
+            }
+        }
+        return $result;
     }
 }
--- tests/RomanNumbersTest.php
+++ tests/RomanNumbersTest.php
@@ -19,6 +19,20 @@ class RomanNumbersTest extends TestCase
     {
         return [
             [1, 'I'],
+            [2, 'II'],
+            [3, 'III'],
+            [4, 'IV'],
+            [5, 'V'],
+            [6, 'VI'],
+            [7, 'VII'],
+            [8, 'VIII'],
+            [9, 'IX'],
+            [10, 'X'],
+            [11, 'XI'],
+            [33, 'XXXIII'],
+            [99, 'XCIX'],
+            [399, 'CCCXCIX'],
+            [999, 'CMXCIX'],
         ];
     }
 }
